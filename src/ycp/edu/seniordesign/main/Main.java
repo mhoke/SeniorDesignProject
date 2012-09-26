85d0be373e63c5f0c8b8ec8dfe83f5425e87fc8a
Nick Brady <nbrady1@ycp.edu>
2012-09-26 17:17:02 -0400

    Fixed build path.

--- src/ycp/edu/seniordesign/main/Main.java
+++ src/ycp/edu/seniordesign/main/Main.java
@@ -4,10 +4,18 @@ import java.sql.SQLException;
 
 import ycp.edu.seniordesign.model.User;
 import ycp.edu.seniordesign.model.persist.Database;
+import ycp.edu.seniordesign.model.persist.PersistenceException;
 
 public class Main {
-	public static void main(String[] args) throws SQLException {
-		Database.getInstance().createAccount("Nick","Password", "nbrady1@ycp.edu", User.PROFESSOR_PROFILE);
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, PersistenceException {
+		User user = new User();
+		user.setUsername("Nick");
+		user.setPassword("password");
+		user.setEmailAddress("nbrady1@ycp.edu");
+		user.setType(User.PROFESSOR_PROFILE);
+		Database.getInstance().createAccount("Nick", "password", "nbrady1@ycp.edu", User.STUDENT_PROFILE);
+		//Database.getInstance().createAccount(user);
+		System.out.println("DONE");
 	}
 		
 }
