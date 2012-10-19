58ecf7e1648af79f21c9e253628183e2603809ad
mhoke <mhoke@ycp.edu>
2012-10-19 15:15:47 -0700

    Added the student gradebook functionality. Need test assignments to fully verify

--- src/ycp/edu/seniordesign/controller/GradebookController.java
+++ src/ycp/edu/seniordesign/controller/GradebookController.java
@@ -1,7 +1,10 @@
 package ycp.edu.seniordesign.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import ycp.edu.seniordesign.model.Assignment;
+import ycp.edu.seniordesign.model.Course;
 import ycp.edu.seniordesign.model.User;
 import ycp.edu.seniordesign.model.persist.PersistenceException;
 import ycp.edu.seniordesign.model.persist.Database;
@@ -13,5 +16,20 @@ public class GradebookController
 	public void setModel(User model)
 	{
 		this.user = model;
+	}
+	
+	public User getModel()
+	{
+		return user;
+	}
+	
+	public Course getCourse(int id) throws SQLException
+	{
+		return Database.getInstance().getCourseById(id);
+	}
+	
+	public ArrayList<Assignment> getStudentAssignments (int id) throws SQLException
+	{
+		return Database.getInstance().getAssignmentsForCourse(id, user.getId());
 	}
 }
---
+++ src/ycp/edu/seniordesign/webapp/StudentGradebookServlet.java
@@ -0,0 +1,60 @@
+package ycp.edu.seniordesign.webapp;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ycp.edu.seniordesign.controller.GradebookController;
+import ycp.edu.seniordesign.model.User;
+import ycp.edu.seniordesign.model.persist.Database;
+
+public class StudentGradebookServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		if(req.getSession().getAttribute("user") == null)
+		{
+			req.getRequestDispatcher("/view/login.jsp").forward(req,resp);
+		}
+		
+		else
+		{
+			req.getRequestDispatcher("/view/studentGradebook.jsp").forward(req, resp);
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		User user = (User) req.getSession().getAttribute("user");
+		
+		if(user == null)
+		{
+			req.getRequestDispatcher("/view/login.jsp");
+		}
+		GradebookController controller = new GradebookController();
+		
+		controller.setModel(user);
+		resp.setContentType("application/json");
+		
+		if(!req.getParameter("id").equals("undefined"))
+		{
+			int courseID = Integer.parseInt(req.getParameter("id"));
+			try 
+			{
+				req.getSession().setAttribute("course", controller.getCourse(courseID));
+				System.out.println(controller.getCourse(courseID).getName());
+				req.getSession().setAttribute("assignments", controller.getStudentAssignments(courseID));
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
