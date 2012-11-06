6e3c3419b8100fc20fb458d53d3b7ee0e14fbef6
Nick Brady <nbrady1@ycp.edu>
2012-11-06 14:32:49 -0500

    registration

--- src/ycp/edu/seniordesign/main/Main.java
+++ src/ycp/edu/seniordesign/main/Main.java
@@ -2,12 +2,21 @@ package ycp.edu.seniordesign.main;
 
 import java.sql.SQLException;
 
+import ycp.edu.seniordesign.model.ComputeGrade;
+import ycp.edu.seniordesign.model.Course;
 import ycp.edu.seniordesign.model.User;
 import ycp.edu.seniordesign.model.persist.Database;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
-		Database.getInstance().createAccount("Nick", "Nick Brady", "password", "nbrady1@ycp.edu", User.PROFESSOR_PROFILE);
+		User student = new User();
+		student.setId(0);
+		Course course = new Course();
+		course.setId(0);
+		ComputeGrade computeGrade = new ComputeGrade();
+		computeGrade.setCourse(course);
+		computeGrade.setStudent(student);
+		computeGrade.computeScore();
 	}
 		
 }
--- src/ycp/edu/seniordesign/webapp/CreateAccountServlet.java
+++ src/ycp/edu/seniordesign/webapp/CreateAccountServlet.java
@@ -1,7 +1,18 @@
 package ycp.edu.seniordesign.webapp;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import javax.mail.AuthenticationFailedException;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,46 +23,85 @@ import ycp.edu.seniordesign.controller.CreateAccountController;
 public class CreateAccountServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
-		if(req.getSession().getAttribute("user") == null)
-		{
-			req.getRequestDispatcher("/view/login.jsp").forward(req, resp);
-		}		
-		else
-		{
-			req.getRequestDispatcher("/view/createAccount.jsp").forward(req, resp);
-		}
+		req.getRequestDispatcher("/view/createAccount.jsp").forward(req, resp);
 	}
-	
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
-		CreateAccountController controller = new CreateAccountController();
-		
-		String username = req.getParameter("usernameBox");
-		String password = req.getParameter("passwordBox");
-		String email = req.getParameter("emailBox");
-		
-		String errorMessage = null;
-		
-		int result = -1;
+		CreateAccountController AcctController = new CreateAccountController();
 
-		if(req.getParameter("createButton") != null)
+
+
+		String errorMessage = null;
+
+		boolean result = false;
+
+		if(req.getParameter("RegisterButton") != null)
 		{
 			System.out.println("Enters here");
 			try
 			{
-				// TODO: should we get a name now that we have added that field to the user class
-				result = controller.createAccount(username, " ", password, email);
+
+				//result = controller.createAccount(username, password, email);
+
+				String Euser = "whiteboardseniordesign@gmail.com";
+				String Epassword = "randompassword";
+				String authCode = AcctController.generateRandomString();
+				//user form data
+				String username = req.getParameter("usernameBox");
+				String name = req.getParameter("nameBox");
+				String emailTo = req.getParameter("emailBox");
+				String message = name +", your random code: " + authCode+ " is for the account: " + username;
+				String subjectLine = "Whiteboard: Registraion Code";
 				
-				if(result == -1)
-				{
-					errorMessage = "Create Account failed";
-				}
-				else
-				{
-					errorMessage = "Create Account successful";
+				//if this username is not found in our main directory than proceed to
+				//add into the inactiveUsers table and store the random string 
+				if(AcctController.addRegistration(username, emailTo, authCode)){
+
+					//preparing to send the message
+					Properties props = new Properties();
+					//props.setProperty("mail.host", "smtp.gmail.com");
+					props.put("mail.smtps.auth", "true");
+					props.setProperty("mail.smtps.port", "465");
+					props.setProperty("mail.smtps.auth", "true");
+					props.setProperty("mail.smtps.starttls.enable", "true");
+
+					//authenicates the gmail account
+					System.out.println("Auth the SMTP server...");
+					Authenticator auth = new SMTPAuthenticator(Euser, Epassword);
+					Session session = Session.getInstance(props, auth);
+					MimeMessage msg = new MimeMessage(session);
+					try {
+						//loads message
+						msg.setText(message);
+						msg.setSubject(subjectLine);
+						//from
+						msg.setFrom(new InternetAddress(Euser));
+						//to
+						msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
+						System.out.println("Attempting to send...");
+
+						Transport t = session.getTransport("smtps"); //.send(msg);
+						try {
+							//sending
+							t.connect("smtp.gmail.com", Euser, Epassword);
+							t.sendMessage(msg, msg.getAllRecipients());
+						} finally {
+							t.close();
+						}
+					} catch (AuthenticationFailedException err) {
+						System.out.println(err);
+
+					} catch (AddressException err) {
+						System.out.println(err);
+
+					} catch (MessagingException err) {
+						System.out.println(err);
+					}
+
 				}
 			}
 			catch(Exception e)
@@ -59,10 +109,22 @@ public class CreateAccountServlet extends HttpServlet
 				System.out.println("Create Account fail");
 			}
 		}
-		
+
 		req.setAttribute("errorMessage", errorMessage);
-		
+
 		req.getRequestDispatcher("/view/login.jsp").forward(req,resp);
+	}
+	private class SMTPAuthenticator extends Authenticator {
+
+		private PasswordAuthentication authentication;
+
+		public SMTPAuthenticator(String Euser, String Epassword) {
+			authentication = new PasswordAuthentication(Euser, Epassword);
+		}
+
+		protected PasswordAuthentication getPasswordAuthentication() {
+			return authentication;
+		}
 	}
 }
 
